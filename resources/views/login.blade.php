511a35fe4584df02ce7433ae92dbe654dabfab7c
Michelle Pauline <michellepaulinn@gmail.com>
2022-06-16 21:06:04 +0700

    update view blade

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,26 @@
 @section('container')
     <h1>Login Page</h1>
     <form>
-
+        <form action="/loginProcess" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" name="email" id="email">
+            {{-- <br> --}}
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" id="poster">
+                {{-- <br> --}}
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" name="rememberMe" id="rememberMe"><label for="rememberMe" class="form-check-label">Remember Me</label>
+                <br>
+            </div>
+            <a href="/register">Don't have an account? Register Now!</a>
+            <br>
+            <input type="submit" class="btn bg-navy text-light" value="LOG IN">
+        </form>
 
     </form>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,11 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    {{-- Google Fonts --}}
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>@yield('title')</title> 
+    <style>
+        .logo{
+            font-family: 'Times New Roman', Times, serif;
+            color: black;
+        }
+    </style>
   </head>
   <body>
-    <header class="py-3 mb-4 border-bottom navbar-light">
-        <div class="mb-2 d-flex flex-row align-items-center justify-content-between">    
+    {{-- <header class="py-3 mb-4 border-bottom navbar-light">
+        <div class="my-2 d-flex flex-row align-items-center justify-content-between">    
             <a href="/" class="col-md-2 mb-2 mb-md-0 text-decoration-none">
                 <h3>Thrift Store</h3>
             </a>
@@ -35,19 +43,58 @@
                 <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
             </ul>
         </div>
-    </header>
+    </header> --}}
 
+    <header>
+        <div class="nav justify-content-between d-flex gap-4 p-4 w-100">
+            <div class="logo nav-item">
+                <h3 class="">Thrift Store</h3>
+            </div>
+            <div class="search nav-item flex-grow-1 px-4 mx-4">
+                <form action="" class="d-flex flex-row" method="GET">
+                    <input type="search" class="form-control pr-5 pl-3 mr-1" placeholder="Celana, Baju, ..." aria-label="Search" name="keyword">
+                    <button class="btn " type="submit">
+                        <span class="material-icons">search</span>
+                    </button>
+                </form>
+            </div>
+            <div class="d-flex nav-item gap-3">
+                <div class="cart px-4 align-baseline">
+                    <span class="material-icons">shopping_cart</span>
+                </div>
+                <div class="profile px-4 ">
+                    <span class="material-icons">person</span>
+                </div>
+            </div>
+        </div>
+        <div class="ctg d-flex col-md-8">
+            <div class="ctg-item">
+                <a href="">Outer</a>
+            </div>
+            <div class="ctg-item">
+                <a href=""> Tops</a>
+            </div>
+            <div class="ctg-item">
+                <a href="">Bottoms</a>
+            </div>
+            <div class="ctg-item">
+                <a href="">Accessories</a>
+            </div>
+        </div>
+    </header>
     <div class="container mt-4">
         @yield('container')
     </div>
     
     <div class="container">
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div class="col-md-4 d-flex align-items-center">
-                <a href="/" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1 text-reset">
-                    <h3>Thrift Store  </h3>
-                </a>
-                <span class="text-muted">  &copy Thrift Company </span>
+            <div class="col-md-4 d-flex align-items-center gap-4">
+                <div>
+                    <a href="/" class="logo">
+                        <h3>Thrift Store  </h3>
+                    </a>
+                </div>
+                <div><span class="text-muted mx-4">  &copy Thrift Company </span></div>
             </div>
       
             <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
