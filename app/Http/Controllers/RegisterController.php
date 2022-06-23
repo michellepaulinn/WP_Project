c316b536d2e4d92c328e54f554d5165fd0a9257c
Chelsea <makanayam749@yahoo.com>
2022-06-23 16:35:00 +0700

    register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,14 +13,17 @@ class RegisterController extends Controller
         return view('register');
     }
     public function register(Request $request){
+        if($request->account_password != $request->account_cPassword){
+            return redirect()->back()->with('error','Password and Confirm Password must match');
+        }
         $user = Account::create([
             'account_name' => $request->account_name,
             'account_email' => $request->account_email,
             'account_password' => $request->account_password,
             'role_id' => '2'
         ]);
-
         if($user){
+
             return redirect('/login');
         }
         return redirect()->back()->with('error','Registration failed!');
