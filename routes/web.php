fa1d9cf837a0963ca03d44056a5d0e0746f5a6e1
jossenclinson <jossenclinson@yahoo.co.id>
2022-06-26 18:51:32 +0700

    menambah middleware VerifyLogin & VerifyLogout

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Middleware\VerifyLogin;
+use App\Http\Middleware\VerifyLogout;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +23,23 @@ use App\Http\Controllers\TransactionController;
 |
 */
 
+// baik sudah login maupun belum login, dapat akses route ke:
 Route::get('/', [PageController::class, 'homepage']);
+Route::get('/search', [ItemController::class, 'searchItem']);
+Route::get('/item/{id}',[ItemController::class, 'itemDetail']);
 
-Route::get('/login', [LoginController::class, 'form']);
-Route::post('/login-process', [LoginController::class, 'login']);
-Route::get('/logout', [LoginController::class, 'logout']);
-Route::get('/register', [RegisterController::class, 'form']);
-Route::post('/register-process', [RegisterController::class, 'register']);
 
+Route::middleware([VerifyLogout::class])->group(function() {
+    // kalau belum login, dapat akses route ke:
+    Route::get('/login', [LoginController::class, 'form']);
+    Route::post('/login-process', [LoginController::class, 'login']);
+    Route::get('/register', [RegisterController::class, 'form']);
+    Route::post('/register-process', [RegisterController::class, 'register']);
+});
+
+Route::middleware([VerifyLogin::class])->group(function() {
+    // kalau sudah login, dapat akses route ke:
+    Route::get('/logout', [LoginController::class, 'logout']);
     //Frontend
     Route::get('manage-item', [ItemController::class, 'view_update']);
     Route::get('add-item', [ItemController::class, 'view_create']);
@@ -37,7 +48,7 @@ Route::post('/register-process', [RegisterController::class, 'register']);
     Route::post('create-item',[ItemController::class,'create_item']);
     Route::post('confirm-payment-process',[ItemController::class, 'confirm']);
 
-Route::get('/cart', [CartController::class, 'viewCart']);
+    Route::get('/cart', [CartController::class, 'viewCart']);
+});
 
-Route::get('/search', [ItemController::class, 'searchItem']);
-Route::get('/item/{id}',[ItemController::class, 'itemDetail']);
+
