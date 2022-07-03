dee97b7c86f73dc983b3dfe6a39df0a324660a06
jossenclinson <jossenclinson@yahoo.co.id>
2022-07-03 14:37:58 +0700

    remove useless function in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,6 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function homepage(){
-        $category = Category::all();
-        $clothes = Item::all();
-
-        return view('homepage', ['category'=>$category, 'clothes'=>$clothes]);
-        // return view('homepage')->with('category', $category)->with('clothes',$clothes);
-    }
-
     public function login(){
         return view('login');
     }
