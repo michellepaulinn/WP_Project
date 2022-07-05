3839f7cf38bf7eb1aa2b958dda2f51ad50c88f32
VincyLie <vincylie23@gmail.com>
2022-07-05 15:35:54 +0700

    Category item UI

--- resources/views/categoryItem.blade.php
+++ resources/views/categoryItem.blade.php
@@ -8,24 +8,24 @@
             <div class="ctg d-flex justify-content-center w-100">
                 @foreach($categories as $ctg)
                     <div class="ctg-item mx-2">
-                        <a href="/category/{{$ctg->id}}">{{$ctg->category_name}}</a>
+                        <a style="color:#396854" href="/category/{{$ctg->id}}">{{$ctg->category_name}}</a>
                     </div>
                 @endforeach
             </div>
         </div>
 
         <div class="newArrival">
-            <div class="catName text-center m-4"><h2 class="title">{{ $items[0]->category->category_name }}</h2></div>
+            <div class="catName text-center m-4"><h2 style="color:#396854;"class="title">{{ $items[0]->category->category_name }}</h2></div>
             <div class="d-flex flex-wrap container m-2 justify-content-between">
                @foreach ($items as $item)
-                <a href="/item/{{ $item->id }}" class="mx-4">
-                <div class="card border-0">
+                <a href="/item/{{ $item->id }}" class="m-3">
+                <div class="card border-1" style="background-color:#ede6db;">
                     @foreach ($images as $image)
                         @if ($image->item_id === $item->id)
-                            <img class="card-img-top" src="/photos/{{$image->item_image}}" alt="Card image cap" width="272" height="240" style="max-width: 272px; max-height: 240;">                       
+                            <img class="card-img-top p-3" src="/photos/{{$image->item_image}}" alt="Card image cap" width="272" height="240" style="width: 272px; height: 240;">                       
                         @endif
                     @endforeach
-                    <div class="card-body text-center">
+                    <div class="card-body text-center" style="color:#396854">
                         <h6 class="card-title">{{ $item->item_name }}</h6>
                         <p class="card-text">{{ number_format($item->item_price) }}</p>
                     </div>
