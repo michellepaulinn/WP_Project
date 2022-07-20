7781d562d5568f83886007b392f101516d55199d
alorentiar <alorentiar03@gmail.com>
2022-07-20 11:56:24 +0700

    update transaction list view

--- resources/views/transaction-list.blade.php
+++ resources/views/transaction-list.blade.php
@@ -5,7 +5,7 @@
 @section ('content')
     <div class="container mx-auto p-5" style="min-height:420px;">
         {{-- {{dd($transaction)}} --}}
-         @foreach ($transaction as $tr)
+         {{-- @foreach ($transaction as $tr)
         <a href="/transaction/{{$tr->id}}">
             <div class="card p-2 m-2 bg-sec">
                 <div class="card-title"> <h5 class="fw-bold p-3"> {{$tr->transactionStatus->status_name}} </h5> </div>
@@ -22,8 +22,24 @@
                     </div>
                  </div>
             </div>
-        </a>
-            
+        </a> --}}
+        @foreach ($transaction as $tr)
+        <div class="card bg-sec m-2">
+            <div class="card-header">
+                <div class="card-title"> <h5 class="fw-bold pt-2 pb-2"> {{$tr->transactionStatus->status_name}} </h5> </div>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Transaction id : {{ $tr->id }}</h5>
+              <br>
+                        @php($total = 0)
+                        @foreach($tr->transactionDetails as $trd)
+                            @php($total += $trd->item->item_price)
+                        @endforeach
+                        {{number_format($total)}}
+                <br>
+              <a href="/transaction/{{$tr->id}}" class="btn btn-primary mt-2 mb-2">Update Order Status</a>
+            </div>
+          </div>
         @endforeach
     </div>    
 @endsection
